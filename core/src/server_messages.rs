034d4d2cc1b3281ef125c83e7403544e5b7bc757
Brian Warner <warner@lothar.com>
2018-03-31 14:58:35 -0700

    switch to serde-json "enum representation" for server messages

--- core/src/server_messages.rs
+++ core/src/server_messages.rs
@@ -1,22 +1,39 @@
+use serde_json;
+use serde_json::{Value, Number};
+
+#[derive(Serialize, Deserialize)]
+struct Nameplate {
+    id: String,
+}
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all="kebab-case")]
-pub struct Bind {
-    #[serde(rename="type")]
-    pub msg_type: String,
-    pub appid: String,
-    pub side: String,
+#[serde(tag="type")]
+pub enum Message {
+    Bind { appid: String, side: String },
+    List,
+    Nameplates { nameplates: Vec<Nameplate> },
+    Allocate,
+    Allocated { nameplate: String },
+    Claim { nameplate: String },
+    Claimed { mailbox: String },
+    Release { nameplate: String }, // TODO: nominally optional
+    Released { },
+    Open { mailbox: String },
+    Add { phase: String, body: String },
+    Message { side: String, phase: String, body: String, id: String },
+    Close { mailbox: String, mood: String },
+    Closed,
+    Ack,
+    Ping { ping: Number }, // actually only integer
+    Pong { pong: Number },
+    //Error { error: String, orig: Message },
 }
 
-pub fn bind(appid: &str, side: &str) -> Bind {
-    Bind{msg_type: "bind".to_owned(),
-         appid: appid.to_string(),
-         side: side.to_string(),
-    }
+pub fn bind(appid: &str, side: &str) -> Message {
+    Message::Bind{appid: appid.to_string(), side: side.to_string() }
 }
-use serde_json;
-use serde_json::Value;
 
-pub fn bind_from_str(s: &str) -> Bind {
+pub fn deserialize(s: &str) -> Message {
     serde_json::from_str(&s).unwrap()
 }
