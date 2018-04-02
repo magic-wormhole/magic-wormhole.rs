854adcc84884c782b692c1cea0595ccbfc4ad4c5
Brian Warner <warner@lothar.com>
2018-04-01 18:45:49 -0700

    wrestle it into compilation

--- core/src/events.rs
+++ core/src/events.rs
@@ -1,41 +1,42 @@
 // Events come into the core, Actions go out of it (to the IO glue layer)
-use super::api::{APIAction, APIEvent, IOAction, IOEvent};
-use super::allocator::AllocatorEvent;
-use super::boss::BossEvent;
-use super::code::CodeEvent;
-use super::input::InputEvent;
-use super::key::KeyEvent;
-use super::lister::ListerEvent;
-use super::mailbox::MailboxEvent;
-use super::nameplate::NameplateEvent;
-use super::order::OrderEvent;
-use super::receive::ReceiveEvent;
-use super::rendezvous::RendezvousEvent;
-use super::send::SendEvent;
-use super::terminator::TerminatorEvent;
+use api::{APIAction, APIEvent, IOAction, IOEvent};
+//use allocator::AllocatorEvent;
+//use boss::BossEvent;
+//use code::CodeEvent;
+//use input::InputEvent;
+//use key::KeyEvent;
+//use lister::ListerEvent;
+//use mailbox::MailboxEvent;
+//use nameplate::NameplateEvent;
+//use order::OrderEvent;
+//use receive::ReceiveEvent;
+use rendezvous::RendezvousEvent;
+//use send::SendEvent;
+//use terminator::TerminatorEvent;
 
+#[derive(Debug, PartialEq)]
 pub enum MachineEvent {
-    Allocator(AllocatorEvent),
-    Boss(BossEvent),
-    Code(CodeEvent),
-    Input(InputEvent),
-    Key(KeyEvent),
-    Lister(ListerEvent),
-    Mailbox(MailboxEvent),
-    Nameplate(NameplateEvent),
-    Order(OrderEvent),
-    Receive(ReceiveEvent),
+    //Allocator(AllocatorEvent),
+    //Boss(BossEvent),
+    //Code(CodeEvent),
+    //Input(InputEvent),
+    //Key(KeyEvent),
+    //Lister(ListerEvent),
+    //Mailbox(MailboxEvent),
+    //Nameplate(NameplateEvent),
+    //Order(OrderEvent),
+    //Receive(ReceiveEvent),
     Rendezvous(RendezvousEvent),
-    Send(SendEvent),
-    Terminator(TerminatorEvent),
+    //Send(SendEvent),
+    //Terminator(TerminatorEvent),
 }
 
-pub enum InboundEvent {
+pub enum InboundEvent { // from IO glue layer
     IO(IOEvent),
     API(APIEvent),
 }
 
-pub enum Action {
+pub enum Action { // to IO glue later
     // outbound
     IO(IOAction),
     API(APIAction),
@@ -47,14 +48,60 @@ pub enum ProcessEvent {
     Machine(MachineEvent),
 }
 
-pub enum ProcessResultEvent {
+impl From<InboundEvent> for ProcessEvent {
+    fn from(r: InboundEvent) -> Self {
+        match r {
+            InboundEvent::API(a) => ProcessEvent::API(a),
+            InboundEvent::IO(a) => ProcessEvent::IO(a),
+        }
+    }
+}
+
+impl From<RendezvousEvent> for ProcessEvent {
+    fn from(r: RendezvousEvent) -> Self {
+        ProcessEvent::Machine(MachineEvent::Rendezvous(r))
+    }
+}
+
+
+pub enum Result { // superset for WormholeCore::execute internals
     API(APIAction),
     IO(IOAction),
     Machine(MachineEvent),
 }
 
-/*
-process_one_event(inbound api, inbound io, machines) -> (outbound api, outbound io, machines)
-execute(inbound api, inbound io) -> (outbound api, outbound io)
+#[derive(Debug, PartialEq)]
+pub enum BossResult { // only Boss is allowed to emit APIActions
+    API(APIAction),
+    Machine(MachineEvent),
+}
 
-*/
+impl From<BossResult> for Result {
+    fn from(r: BossResult) -> Self {
+        match r {
+            BossResult::API(a) => Result::API(a),
+            BossResult::Machine(e) => Result::Machine(e),
+        }
+    }
+}
+
+pub enum RendezvousResult { // only Rendezvous is allowed to emit IOAction
+    IO(IOAction),
+    Machine(MachineEvent),
+}
+
+impl From<RendezvousResult> for Result {
+    fn from(r: RendezvousResult) -> Self {
+        match r {
+            RendezvousResult::IO(a) => Result::IO(a),
+            RendezvousResult::Machine(e) => Result::Machine(e),
+        }
+    }
+}
+
+// other machines are only allowed to emit MachineEvent
+impl From<MachineEvent> for Result {
+    fn from(e: MachineEvent) -> Self {
+        Result::Machine(e)
+    }
+}
--- core/src/traits.rs
+++ core/src/traits.rs
@@ -1,13 +1,8 @@
-use std::collections::HashMap;
-
-pub enum Action {
-    IO(IOEvent),
-    API(OutboundAPIEvent),
-}
+use events::{InboundEvent, Action};
 
 pub trait Core {
     fn start(&mut self) -> Vec<Action>;
-    fn execute(&mut self, event: InboundAPIEvent) -> Vec<Action>;
+    fn execute(&mut self, event: InboundEvent) -> Vec<Action>;
 
     fn derive_key(&mut self, purpose: &str, length: u8) -> Vec<u8>;
 }
