1dfb63f5cd5e84b7caa9896ea30a311de73ab4cf
Brian Warner <warner@lothar.com>
2018-04-02 18:54:10 -0700

    add stub for Allocator

--- core/src/allocator.rs
+++ core/src/allocator.rs
@@ -1,5 +1,18 @@
-pub enum AllocatorEvent {
-    Connected,
-    Lost,
-    RxAllocated,
+use events::Event;
+use events::Event::{A_Connected, A_Lost, A_RxAllocated};
+pub struct Allocator {}
+
+pub fn new() -> Allocator {
+    Allocator {}
+}
+
+impl Allocator {
+    pub fn process(&mut self, event: Event) -> Vec<Event> {
+        match event {
+            A_Connected => vec![],
+            A_Lost => vec![],
+            A_RxAllocated => vec![],
+            _ => panic!(),
+        }
+    }
 }
