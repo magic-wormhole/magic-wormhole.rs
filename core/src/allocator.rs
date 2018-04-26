9dbd91c42546e062c949443f43f78ab0afa17f53
Vasudev Kamath <vasudev@copyninja.info>
2018-04-26 17:18:29 +0530

    allocator machine: initial implementation.

--- core/src/allocator.rs
+++ core/src/allocator.rs
@@ -1,22 +1,120 @@
-use events::Events;
+use events::{Events, Wordlist};
 // we process these
-use events::AllocatorEvent;
+use events::AllocatorEvent::{self, Allocate, Connected, Lost, RxAllocated};
 // we emit these
+use events::RendezvousEvent::TxAllocate as RC_TxAllocate;
+use events::CodeEvent::Allocated as C_Allocated;
 
-pub struct Allocator {}
+pub struct Allocator {
+    state: State,
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum State {
+    S0A_idle_disconnected,
+    S0B_idle_connected,
+    S1A_allocating_disconnected(u8, Wordlist),
+    S1B_allocating_connected(u8, Wordlist),
+    S2_done,
+}
 
 impl Allocator {
     pub fn new() -> Allocator {
-        Allocator {}
+        Allocator {
+            state: State::S0A_idle_disconnected,
+        }
     }
 
     pub fn process(&mut self, event: AllocatorEvent) -> Events {
-        use events::AllocatorEvent::*;
+        use self::State::*;
+        let (newstate, actions) = match self.state {
+            S0A_idle_disconnected => self.do_idle_disconnected(event),
+            S0B_idle_connected => self.do_idle_connected(event),
+            S1A_allocating_disconnected(..) => {
+                self.do_allocating_disconnected(event)
+            }
+            S1B_allocating_connected(..) => self.do_allocating_connected(event),
+            S2_done => (self.state, events![]),
+        };
+
+        self.state = newstate;
+        actions
+    }
+
+    fn do_idle_disconnected(
+        &mut self,
+        event: AllocatorEvent,
+    ) -> (State, Events) {
         match event {
-            Connected => events![],
-            Lost => events![],
-            Allocate(length, wordlist) => events![],
-            RxAllocated => events![],
+            Connected => (State::S0B_idle_connected, events![]),
+            Allocate(_length, _wordlist) => (
+                State::S1A_allocating_disconnected(_length, _wordlist),
+                events![],
+            ),
+            _ => (self.state, events![]),
         }
     }
+
+    fn do_idle_connected(&mut self, event: AllocatorEvent) -> (State, Events) {
+        match event {
+            Lost => (State::S0A_idle_disconnected, events![]),
+            Allocate(_length, _wordlist) => (
+                State::S1B_allocating_connected(_length, _wordlist),
+                events![RC_TxAllocate],
+            ),
+            _ => (self.state, events![]),
+        }
+    }
+
+    fn do_allocating_disconnected(
+        &self,
+        event: AllocatorEvent,
+    ) -> (State, Events) {
+        match event {
+            Connected => {
+                if let State::S1A_allocating_disconnected(_length, _wordlist) =
+                    self.state
+                {
+                    (
+                        State::S1B_allocating_connected(_length, _wordlist),
+                        events![RC_TxAllocate],
+                    )
+                } else {
+                    panic!();
+                }
+            }
+            _ => (self.state, events![]),
+        }
+    }
+
+    fn do_allocating_connected(
+        &mut self,
+        event: AllocatorEvent,
+    ) -> (State, Events) {
+        match event {
+            Lost => {
+                if let State::S1B_allocating_connected(_length, _wordlist) =
+                    self.state
+                {
+                    (
+                        State::S1A_allocating_disconnected(_length, _wordlist),
+                        events![],
+                    )
+                } else {
+                    panic!();
+                }
+            }
+            RxAllocated(nameplate) => {
+                let code = "purple-sausages".to_string(); // TODO: implement choose_words function
+                (State::S2_done, events![C_Allocated(nameplate, code)])
+            }
+            _ => (self.state, events![]),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::State::*;
+    use super::Allocator;
 }
