b7af71dae5719657e04a6d91876472207367bbfe
Brian Warner <warner@lothar.com>
2018-05-15 15:10:07 -0400

    wordlist: new API

--- core/src/wordlist.rs
+++ core/src/wordlist.rs
@@ -1,7 +1,6 @@
+use rand::{OsRng, Rng};
 use serde_json::{self, Value};
-use std::collections::{HashMap, HashSet};
-
-use util::{bytes_to_hexstr, random_bytes};
+use std::collections::HashMap;
 
 #[derive(Debug, PartialEq)]
 pub struct Wordlist {
@@ -19,23 +18,89 @@ impl Wordlist {
 
     pub fn get_completions(
         &self,
-        _prefix: &str,
-        _num_words: usize,
+        prefix: &str,
+        num_words: usize,
     ) -> Vec<String> {
-        vec![] // TODO
+        let count_dashes = prefix.matches('-').count();
+        let mut completions = Vec::new();
+        let words = &self.words[count_dashes % self.words.len()];
+
+        let last_partial_word = prefix.split('-').last();
+        let lp = if let Some(w) = last_partial_word {
+            w.len()
+        } else {
+            0
+        };
+
+        for word in words {
+            let mut suffix: String = prefix.to_owned();
+            if word.starts_with(last_partial_word.unwrap()) {
+                if lp == 0 {
+                    suffix.push_str(&word);
+                } else {
+                    let p = prefix.len() - lp;
+                    suffix.split_off(p as usize);
+                    suffix.push_str(&word);
+                }
+
+                if count_dashes + 1 < num_words {
+                    suffix.push_str("-");
+                }
+
+                completions.push(suffix);
+            }
+        }
+        completions.sort();
+        completions
     }
-    pub fn choose_words(&self, _num_words: usize) -> String {
-        "".to_string() // TODO
+
+    pub fn choose_words(&self, num_words: usize) -> String {
+        let mut rng = OsRng::new().unwrap();
+        let components: Vec<String>;
+        components = self.words
+            .iter()
+            .cycle()
+            .take(num_words)
+            .map(|words| rng.choose(words).unwrap().to_string())
+            .collect();
+        components.join("-")
     }
+}
+
+pub fn load_pgpwords() -> Vec<Vec<String>> {
+    let raw_words_value: Value =
+        serde_json::from_str(include_str!("pgpwords.json")).unwrap();
+    let raw_words = raw_words_value.as_object().unwrap();
+    let mut even_words: Vec<String> = Vec::with_capacity(256);
+    even_words.resize(256, "".to_string());
+    let mut odd_words: Vec<String> = Vec::with_capacity(256);
+    odd_words.resize(256, "".to_string());
+    for (index_str, values) in raw_words.iter() {
+        let index = u8::from_str_radix(index_str, 16).unwrap() as usize;
+        //println!("values {:?}", values.get(0).unwrap().as_str().unwrap().to_string());
+        even_words[index] = values
+            .get(1)
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .to_lowercase()
+            .to_string();
+        odd_words[index] = values
+            .get(0)
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .to_lowercase()
+            .to_string();
+    }
+
+    vec![even_words, odd_words]
 }
 
 pub fn default_wordlist() -> Wordlist {
     Wordlist {
         length: 2, // TODO: accept and remember a length, not hardcoded
-        words: vec![
-            vec!["word1".to_string(), "word2".to_string()],
-            vec!["word3".to_string(), "word4".to_string()],
-        ],
+        words: load_pgpwords(),
     }
 }
 
@@ -86,86 +151,112 @@ impl PGPWordlist {
             _byte_odd_words: odd_words,
         }
     }
-
-    #[allow(dead_code)]
-    pub fn get_completions(
-        &self,
-        prefix: &str,
-        num_words: usize,
-    ) -> HashSet<String> {
-        let count_dashes = prefix.matches('-').count();
-        let words;
-        let mut completions: HashSet<String> = HashSet::new();
-
-        if count_dashes % 2 == 0 {
-            words = self._byte_odd_words
-                .values()
-                .map(String::to_string)
-                .collect::<Vec<String>>();
-        } else {
-            words = self._byte_even_words
-                .values()
-                .map(String::to_string)
-                .collect::<Vec<String>>();
-        }
-
-        let last_partial_word = prefix.split('-').last();
-        let lp = if let Some(w) = last_partial_word {
-            w.len()
-        } else {
-            0
-        };
-
-        for word in words {
-            let mut suffix: String = prefix.to_owned();
-            if word.starts_with(last_partial_word.unwrap()) {
-                if lp == 0 {
-                    suffix.push_str(&word);
-                } else {
-                    let p = prefix.len() - lp;
-                    suffix.split_off(p as usize);
-                    suffix.push_str(&word);
-                }
-
-                if count_dashes + 1 < num_words {
-                    suffix.push_str("-");
-                }
-
-                completions.insert(suffix);
-            }
-        }
-
-        completions
-    }
-
-    #[allow(dead_code)]
-    pub fn choose_words(&self, length: u8) -> String {
-        let mut rnd: [u8; 1] = [0; 1];
-        let mut words: Vec<String> = Vec::new();
-        for i in 0..length {
-            random_bytes(&mut rnd);
-            let key = bytes_to_hexstr(&rnd).to_uppercase();
-            if i % 2 == 0 {
-                let word = self._byte_odd_words[&key].as_str();
-                words.push(word.to_string());
-            } else {
-                let word = self._byte_even_words[&key].as_str();
-                words.push(word.to_string());
-            }
-        }
-
-        words.join("-")
-    }
 }
 
 #[cfg(test)]
 mod test {
-    use super::PGPWordlist;
-    //use std::collections::HashSet;
+    use super::*;
 
     #[test]
-    fn test_completions() {
-        let w = PGPWordlist::new();
+    fn test_load_words() {
+        let w = load_pgpwords();
+        assert_eq!(w.len(), 2);
+        assert_eq!(w[0][0], "adroitness");
+        assert_eq!(w[1][0], "aardvark");
+        assert_eq!(w[0][255], "yucatan");
+        assert_eq!(w[1][255], "zulu");
+    }
+
+    #[test]
+    fn test_default_wordlist() {
+        let d = default_wordlist();
+        assert_eq!(d.words.len(), 2);
+        assert_eq!(d.words[0][0], "adroitness");
+        assert_eq!(d.words[1][0], "aardvark");
+        assert_eq!(d.words[0][255], "yucatan");
+        assert_eq!(d.words[1][255], "zulu");
+    }
+
+    fn vecstrings(all: &str) -> Vec<String> {
+        all.split_whitespace()
+            .map(|s| {
+                if s == "." {
+                    "".to_string()
+                } else {
+                    s.to_string()
+                }
+            })
+            .collect()
+    }
+
+    #[test]
+    fn test_completion() {
+        let words: Vec<Vec<String>> = vec![
+            vecstrings("purple green yellow"),
+            vecstrings("sausages seltzer snobol"),
+        ];
+
+        let w = Wordlist::new(2, words);
+        assert_eq!(
+            w.get_completions("", 2),
+            vec!["green-", "purple-", "yellow-"]
+        );
+        assert_eq!(w.get_completions("pur", 2), vec!["purple-"]);
+        assert_eq!(
+            w.get_completions("blu", 2),
+            Vec::<String>::new()
+        );
+        assert_eq!(
+            w.get_completions("purple-sa", 2),
+            vec!["purple-sausages"]
+        );
+    }
+
+    #[test]
+    fn test_choose_words() {
+        let few_words: Vec<Vec<String>> =
+            vec![vecstrings("purple"), vecstrings("sausages")];
+
+        let w = Wordlist::new(2, few_words);
+        assert_eq!(w.choose_words(2), "purple-sausages");
+        assert_eq!(w.choose_words(3), "purple-sausages-purple");
+        assert_eq!(
+            w.choose_words(4),
+            "purple-sausages-purple-sausages"
+        );
+    }
+
+    #[test]
+    fn test_choose_more_words() {
+        let more_words: Vec<Vec<String>> = vec![
+            vecstrings("purple yellow"),
+            vecstrings("sausages"),
+        ];
+
+        let expected2 = vecstrings("purple-sausages yellow-sausages");
+        let expected3: Vec<String> = vec![
+            "purple-sausages-purple",
+            "yellow-sausages-purple",
+            "purple-sausages-yellow",
+            "yellow-sausages-yellow",
+        ].iter()
+            .map(|s| s.to_string())
+            .collect();
+
+        let w = Wordlist::new(2, more_words.clone());
+        for _ in 0..20 {
+            assert!(expected2.contains(&w.choose_words(2)));
+        }
+
+        let w = Wordlist::new(3, more_words.clone());
+        for _ in 0..20 {
+            assert!(expected3.contains(&w.choose_words(3)));
+        }
+    }
+
+    #[test]
+    fn test_default_completions() {
+        let w = default_wordlist();
         let c = w.get_completions("ar", 2);
         assert_eq!(c.len(), 2);
         assert!(c.contains(&String::from("article-")));
@@ -179,30 +270,28 @@ mod test {
         assert_eq!(c.len(), 256);
 
         let c = w.get_completions("armistice-ba", 2);
-
-        assert_eq!(c.len(), 4);
-        assert!(c.contains("armistice-baboon"));
-        assert!(c.contains("armistice-backfield"));
-        assert!(c.contains("armistice-backward"));
-        assert!(c.contains("armistice-banjo"));
+        assert_eq!(
+            c,
+            vec![
+                "armistice-baboon",
+                "armistice-backfield",
+                "armistice-backward",
+                "armistice-banjo",
+            ]
+        );
 
         let c = w.get_completions("armistice-ba", 3);
-        assert_eq!(c.len(), 4);
-        assert!(c.contains("armistice-baboon-"));
-        assert!(c.contains("armistice-backfield-"));
-        assert!(c.contains("armistice-backward-"));
-        assert!(c.contains("armistice-banjo-"));
+        assert_eq!(
+            c,
+            vec![
+                "armistice-baboon-",
+                "armistice-backfield-",
+                "armistice-backward-",
+                "armistice-banjo-",
+            ]
+        );
 
         let c = w.get_completions("armistice-baboon", 4);
-        assert_eq!(c.len(), 1);
-        assert!(c.contains("armistice-baboon-"));
-    }
-
-    #[test]
-    fn test_choose_words() {
-        let w = PGPWordlist::new();
-        let words = w.choose_words(2);
-        assert!(words.contains('-'));
-        assert_eq!(words.matches('-').count(), 1);
+        assert_eq!(c, vec!["armistice-baboon-"]);
     }
 }
