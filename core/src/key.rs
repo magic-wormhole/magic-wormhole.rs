f1d31b9adca18730476bd62aab562db673cca465
Brian Warner <warner@lothar.com>
2018-08-20 22:20:51 -0700

    update deps: hkdf-0.6.0

--- core/src/key.rs
+++ core/src/key.rs
@@ -181,7 +181,9 @@ fn sha256_digest(input: &[u8]) -> Vec<u8> {
 
 pub fn derive_key(key: &[u8], purpose: &[u8], length: usize) -> Vec<u8> {
     let hk = Hkdf::<Sha256>::extract(None, key);
-    hk.expand(purpose, length)
+    let mut v = vec![0; length];
+    hk.expand(purpose, &mut v).unwrap();
+    v
 }
 
 pub fn derive_phase_key(side: &str, key: &Key, phase: &str) -> Vec<u8> {
