dc01346bcadc31c815a90ed898140b98c0c34da7
Brian Warner <warner@lothar.com>
2018-12-20 17:25:17 -0500

    add rx args

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,6 +5,7 @@ use clap::{
 
 fn main() {
     let send = SubCommand::with_name("send")
+        .aliases(&["tx"])
         .arg(
             Arg::with_name("zero")
                 .short("0")
@@ -42,7 +43,60 @@ fn main() {
                 .value_name("MESSAGE")
                 .help("send a text message, not a file"),
         );
-    let receive = SubCommand::with_name("receive");
+    let receive = SubCommand::with_name("receive")
+        .aliases(&["rx"])
+        .arg(
+            Arg::with_name("zero")
+                .short("0")
+                .help("enable no-code anything-goes mode"),
+        )
+        .arg(
+            Arg::with_name("code-length")
+                .short("c")
+                .long("code-length")
+                .takes_value(true)
+                .value_name("NUMWORDS")
+                .help("length of code (in bytes/words)"),
+        )
+        .arg(
+            Arg::with_name("verify")
+                .short("v")
+                .long("verify")
+                .help("display verification string (and wait for approval)"),
+        )
+        .arg(
+            Arg::with_name("hide-progress")
+                .long("hide-progress")
+                .help("supress progress-bar display"),
+        )
+        .arg(
+            Arg::with_name("no-listen")
+                .long("no-listen")
+                .help("(debug) don't open a listening socket for Transit"),
+        )
+        .arg(
+            Arg::with_name("only-text")
+                .short("t")
+                .long("only-text")
+                .help("refuse file transfers, only accept text messages"),
+        )
+        .arg(
+            Arg::with_name("accept-file")
+                .long("accept-file")
+                .help("accept file transfer without asking for confirmation"),
+        )
+        .arg(
+            Arg::with_name("output-file")
+                .short("o")
+                .long("output-file")
+                .takes_value(true)
+                .value_name("FILENAME|DIRNAME")
+                .help("The file or directory to create, overriding the name suggested by the sender"),
+        )
+        .arg(
+            Arg::with_name("code")
+                .help("provide code as argument, rather than typing it interactively")
+        );
 
     let matches = App::new(crate_name!())
         .version(crate_version!())
