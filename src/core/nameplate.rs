98618d8a1ab5517fddf0c52e1b98138e1d8388ce
Brian Warner <warner@lothar.com>
2019-06-21 10:51:59 -0700

    nameplate: simplify

--- src/core/nameplate.rs
+++ src/core/nameplate.rs
@@ -34,226 +34,130 @@ enum State {
 }
 
 pub(crate) struct NameplateMachine {
-    state: State,
+    state: Option<State>,
 }
 
 impl NameplateMachine {
     pub fn new() -> NameplateMachine {
-        NameplateMachine { state: State::S0A }
+        NameplateMachine {
+            state: Some(State::S0A),
+        }
     }
 
     pub fn process(&mut self, event: NameplateEvent) -> Events {
         use self::State::*;
-        let (newstate, actions) = match self.state {
-            S0A => self.do_s0a(event),
-            S0B => self.do_s0b(event),
-            S1A(ref nameplate) => self.do_s1a(&nameplate, event),
-            S2A(ref nameplate) => self.do_s2a(&nameplate, event),
-            S2B(ref nameplate) => self.do_s2b(&nameplate, event),
-            S3A(ref nameplate) => self.do_s3a(&nameplate, event),
-            S3B(ref nameplate) => self.do_s3b(&nameplate, event),
-            S4A(ref nameplate) => self.do_s4a(&nameplate, event),
-            S4B(ref nameplate) => self.do_s4b(&nameplate, event),
-            S5 => self.do_s5(event),
-        };
-        if let Some(s) = newstate {
-            self.state = s;
-        }
+        use NameplateEvent::*;
+        let old_state = self.state.take().unwrap();
+        let mut actions = Events::new();
+        self.state = Some(match old_state {
+            S0A => match event {
+                Connected => S0B,
+                SetNameplate(nameplate) => {
+                    // TODO: validate_nameplate(nameplate)
+                    S1A(nameplate.clone())
+                }
+                Close => {
+                    actions.push(T_NameplateDone);
+                    S5
+                }
+                _ => panic!(),
+            },
+            S0B => match event {
+                Lost => S0A,
+                SetNameplate(nameplate) => {
+                    // TODO: validate_nameplate(nameplate)
+                    actions.push(RC_TxClaim(nameplate.clone()));
+                    S2B(nameplate.clone())
+                }
+                Close => {
+                    actions.push(T_NameplateDone);
+                    S5
+                }
+                _ => panic!(),
+            },
+            S1A(ref nameplate) => match event {
+                Connected => {
+                    actions.push(RC_TxClaim(nameplate.clone()));
+                    S2B(nameplate.clone())
+                }
+                Close => {
+                    actions.push(T_NameplateDone);
+                    S5
+                }
+                _ => panic!(),
+            },
+            S2A(ref nameplate) => match event {
+                Connected => {
+                    actions.push(RC_TxClaim(nameplate.clone()));
+                    S2B(nameplate.clone())
+                }
+                Close => S4A(nameplate.clone()),
+                _ => panic!(),
+            },
+            S2B(ref nameplate) => match event {
+                Lost => S2A(nameplate.clone()),
+                RxClaimed(mailbox) => {
+                    // TODO: use nameplate attributes to pick which wordlist we use
+                    let wordlist = Arc::new(default_wordlist(2)); // TODO: num_words
+                    actions.push(I_GotWordlist(wordlist));
+                    actions.push(M_GotMailbox(mailbox));
+                    S3B(nameplate.clone())
+                }
+                Close => {
+                    actions.push(RC_TxRelease(nameplate.clone()));
+                    S4B(nameplate.clone())
+                }
+                _ => panic!(),
+            },
+            S3A(ref nameplate) => match event {
+                Connected => S3B(nameplate.clone()),
+                Close => S4A(nameplate.clone()),
+                _ => panic!(),
+            },
+            S3B(ref nameplate) => match event {
+                Lost => S3A(nameplate.clone()),
+                Release => {
+                    actions.push(RC_TxRelease(nameplate.clone()));
+                    S4B(nameplate.clone())
+                }
+                Close => {
+                    actions.push(RC_TxRelease(nameplate.clone()));
+                    S4B(nameplate.clone())
+                }
+                _ => panic!(),
+            },
+            S4A(ref nameplate) => match event {
+                Connected => {
+                    actions.push(RC_TxRelease(nameplate.clone()));
+                    S4B(nameplate.clone())
+                }
+                Lost => old_state,
+                Close => old_state,
+                _ => panic!(),
+            },
+            S4B(ref nameplate) => match event {
+                Connected => {
+                    actions.push(RC_TxRelease(nameplate.clone()));
+                    S4B(nameplate.clone())
+                }
+                Lost => S4A(nameplate.clone()),
+                RxClaimed(_mailbox) => old_state,
+                RxReleased => {
+                    actions.push(T_NameplateDone);
+                    S5
+                }
+                Release => old_state,
+                Close => old_state,
+                _ => panic!(),
+            },
+            S5 => match event {
+                Connected => old_state,
+                Lost => old_state,
+                Release => old_state,
+                Close => old_state,
+                _ => panic!(),
+            },
+        });
         actions
-    }
-
-    fn do_s0a(&self, event: NameplateEvent) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => (Some(State::S0B), events![]),
-            Lost => panic!(),
-            RxClaimed(_mailbox) => panic!(),
-            RxReleased => panic!(),
-            SetNameplate(nameplate) => {
-                // TODO: validate_nameplate(nameplate)
-                (Some(State::S1A(nameplate.clone())), events![])
-            }
-            Release => panic!(),
-            Close => (Some(State::S5), events![T_NameplateDone]),
-        }
-    }
-
-    fn do_s0b(&self, event: NameplateEvent) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => panic!(),
-            Lost => (Some(State::S0A), events![]),
-            RxClaimed(_mailbox) => panic!(),
-            RxReleased => panic!(),
-            SetNameplate(nameplate) => {
-                // TODO: validate_nameplate(nameplate)
-                (
-                    Some(State::S2B(nameplate.clone())),
-                    events![RC_TxClaim(nameplate.clone())],
-                )
-            }
-            Release => panic!(),
-            Close => (Some(State::S5), events![T_NameplateDone]),
-        }
-    }
-
-    fn do_s1a(
-        &self,
-        nameplate: &Nameplate,
-        event: NameplateEvent,
-    ) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => (
-                Some(State::S2B(nameplate.clone())),
-                events![RC_TxClaim(nameplate.clone())],
-            ),
-            Lost => panic!(),
-            RxClaimed(_mailbox) => panic!(),
-            RxReleased => panic!(),
-            SetNameplate(_nameplate) => panic!(),
-            Release => panic!(),
-            Close => (Some(State::S5), events![T_NameplateDone]),
-        }
-    }
-
-    fn do_s2a(
-        &self,
-        nameplate: &Nameplate,
-        event: NameplateEvent,
-    ) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => (
-                Some(State::S2B(nameplate.clone())),
-                events![RC_TxClaim(nameplate.clone())],
-            ),
-            Lost => panic!(),
-            RxClaimed(_mailbox) => panic!(),
-            RxReleased => panic!(),
-            SetNameplate(_nameplate) => panic!(),
-            Release => panic!(),
-            Close => (Some(State::S4A(nameplate.clone())), events![]),
-        }
-    }
-
-    fn do_s2b(
-        &self,
-        nameplate: &Nameplate,
-        event: NameplateEvent,
-    ) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => panic!(),
-            Lost => (Some(State::S2A(nameplate.clone())), events![]),
-            RxClaimed(mailbox) => {
-                // TODO: use nameplate attributes to pick which wordlist we use
-                let wordlist = Arc::new(default_wordlist(2)); // TODO: num_words
-                (
-                    Some(State::S3B(nameplate.clone())),
-                    events![I_GotWordlist(wordlist), M_GotMailbox(mailbox)],
-                )
-            }
-            RxReleased => panic!(),
-            SetNameplate(_nameplate) => panic!(),
-            Release => panic!(),
-            Close => (
-                Some(State::S4B(nameplate.clone())),
-                events![RC_TxRelease(nameplate.clone())],
-            ),
-        }
-    }
-
-    fn do_s3a(
-        &self,
-        nameplate: &Nameplate,
-        event: NameplateEvent,
-    ) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => (Some(State::S3B(nameplate.clone())), events![]),
-            Lost => panic!(),
-            RxClaimed(_mailbox) => panic!(),
-            RxReleased => panic!(),
-            SetNameplate(_nameplate) => panic!(),
-            Release => panic!(),
-            Close => (Some(State::S4A(nameplate.clone())), events![]),
-        }
-    }
-
-    fn do_s3b(
-        &self,
-        nameplate: &Nameplate,
-        event: NameplateEvent,
-    ) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => panic!(),
-            Lost => (Some(State::S3A(nameplate.clone())), events![]),
-            RxClaimed(_mailbox) => panic!(),
-            RxReleased => panic!(),
-            SetNameplate(_nameplate) => panic!(),
-            Release => (
-                Some(State::S4B(nameplate.clone())),
-                events![RC_TxRelease(nameplate.clone())],
-            ),
-            Close => (
-                Some(State::S4B(nameplate.clone())),
-                events![RC_TxRelease(nameplate.clone())],
-            ),
-        }
-    }
-
-    fn do_s4a(
-        &self,
-        nameplate: &Nameplate,
-        event: NameplateEvent,
-    ) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => (
-                Some(State::S4B(nameplate.clone())),
-                events![RC_TxRelease(nameplate.clone())],
-            ),
-            Lost => (None, events![]),
-            RxClaimed(_mailbox) => panic!(),
-            RxReleased => panic!(),
-            SetNameplate(_nameplate) => panic!(),
-            Release => panic!(),
-            Close => (None, events![]),
-        }
-    }
-
-    fn do_s4b(
-        &self,
-        nameplate: &Nameplate,
-        event: NameplateEvent,
-    ) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => (
-                Some(State::S4B(nameplate.clone())),
-                events![RC_TxRelease(nameplate.clone())],
-            ),
-            Lost => (Some(State::S4A(nameplate.clone())), events![]),
-            RxClaimed(_mailbox) => (None, events![]),
-            RxReleased => (Some(State::S5), events![T_NameplateDone]),
-            SetNameplate(_nameplate) => panic!(),
-            Release => (None, events![]),
-            Close => (None, events![]),
-        }
-    }
-
-    fn do_s5(&self, event: NameplateEvent) -> (Option<State>, Events) {
-        use super::events::NameplateEvent::*;
-        match event {
-            Connected => (None, events![]),
-            Lost => (None, events![]),
-            RxClaimed(_mailbox) => panic!(),
-            RxReleased => panic!(),
-            SetNameplate(_nameplate) => panic!(),
-            Release => (None, events![]),
-            Close => (None, events![]),
-        }
     }
 }
