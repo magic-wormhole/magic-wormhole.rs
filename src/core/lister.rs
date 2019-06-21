dd654979737c3fe9be6ab1174f21650737f49403
Brian Warner <warner@lothar.com>
2019-06-20 22:34:06 -0700

    lister: simplify

--- src/core/lister.rs
+++ src/core/lister.rs
@@ -7,7 +7,7 @@ use super::events::ListerEvent::*;
 use super::events::RendezvousEvent::TxList as RC_TxList;
 
 pub struct ListerMachine {
-    state: State,
+    state: Option<State>,
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -28,58 +28,61 @@ enum State {
 
 impl ListerMachine {
     pub fn new() -> ListerMachine {
-        ListerMachine { state: State::S0A }
+        ListerMachine {
+            state: Some(State::S0A),
+        }
     }
 
     pub fn process(&mut self, event: ListerEvent) -> Events {
         use self::State::*;
-        let (newstate, actions) = match self.state {
-            S0A => self.do_s0a(&event),
-            S0B => self.do_s0b(event),
-            S1A => self.do_s1a(&event),
-            S1B => self.do_s1b(event),
-        };
-
-        self.state = newstate;
+        let old_state = self.state.take().unwrap();
+        let mut actions = Events::new();
+        self.state = Some(match old_state {
+            S0A => match event {
+                Connected => S0B,
+                Refresh => S1A,
+                _ => old_state,
+            },
+            S0B => match event {
+                Refresh => {
+                    actions.push(RC_TxList);
+                    S1B
+                }
+                Lost => S0A,
+                RxNameplates(nids) => {
+                    // We didn't explicitly need the nameplates, but we got
+                    // them anyway for some reason. Give them to Input so
+                    // they'll be ready in case user wants completion.
+                    actions.push(I_GotNameplates(nids.clone()));
+                    old_state
+                }
+                Connected => old_state,
+            },
+            S1A => match event {
+                Connected => {
+                    actions.push(RC_TxList);
+                    S1B
+                }
+                Refresh => old_state,
+                Lost => old_state,
+                RxNameplates(_) => {
+                    panic!("not connected, shouldn't get nameplates")
+                }
+            },
+            S1B => match event {
+                Lost => S1A,
+                Refresh => {
+                    actions.push(RC_TxList);
+                    old_state
+                }
+                RxNameplates(nids) => {
+                    actions.push(I_GotNameplates(nids.clone()));
+                    S0B
+                }
+                Connected => old_state,
+            },
+        });
         actions
-    }
-
-    fn do_s0a(&self, event: &ListerEvent) -> (State, Events) {
-        match *event {
-            Connected => (State::S0B, events![]),
-            Refresh => (State::S1A, events![]),
-            _ => (State::S0A, events![]),
-        }
-    }
-
-    fn do_s0b(&self, event: ListerEvent) -> (State, Events) {
-        match event {
-            Refresh => (State::S1B, events![RC_TxList]),
-            Lost => (State::S0A, events![]),
-            RxNameplates(nids) => {
-                (State::S0B, events![I_GotNameplates(nids.clone())])
-            }
-            Connected => (State::S0B, events![]),
-        }
-    }
-
-    fn do_s1a(&self, event: &ListerEvent) -> (State, Events) {
-        match event {
-            Connected => (State::S1B, events![RC_TxList]),
-            Refresh => (State::S1B, events![RC_TxList]),
-            _ => (State::S1B, events![]),
-        }
-    }
-
-    fn do_s1b(&self, event: ListerEvent) -> (State, Events) {
-        match event {
-            Lost => (State::S1A, events![]),
-            Refresh => (State::S1B, events![RC_TxList]),
-            RxNameplates(nids) => {
-                (State::S0B, events![I_GotNameplates(nids.clone())])
-            }
-            Connected => (State::S1B, events![]),
-        }
     }
 }
 
@@ -96,15 +99,15 @@ mod test {
     fn test_lister() {
         let mut lister = ListerMachine::new();
 
-        assert_eq!(lister.state, State::S0A);
+        assert_eq!(lister.state, Some(State::S0A));
 
         assert_eq!(lister.process(Connected), events![]);
-        assert_eq!(lister.state, State::S0B);
+        assert_eq!(lister.state, Some(State::S0B));
 
         assert_eq!(lister.process(Lost), events![]);
-        assert_eq!(lister.state, State::S0A);
+        assert_eq!(lister.state, Some(State::S0A));
 
-        lister.state = State::S0B;
+        lister.state = Some(State::S0B);
         let nameplates: Vec<Nameplate> = vec!["3"]
             .into_iter()
             .map(|s| Nameplate(s.to_string()))
@@ -113,12 +116,12 @@ mod test {
             lister.process(RxNameplates(nameplates.clone())),
             events![GotNameplates(nameplates)]
         );
-        assert_eq!(lister.state, State::S0B);
+        assert_eq!(lister.state, Some(State::S0B));
 
         assert_eq!(lister.process(Refresh), events![TxList]);
-        assert_eq!(lister.state, State::S1B);
+        assert_eq!(lister.state, Some(State::S1B));
 
         assert_eq!(lister.process(Refresh), events![TxList]);
-        assert_eq!(lister.state, State::S1B);
+        assert_eq!(lister.state, Some(State::S1B));
     }
 }
