a29858f095e96b1cc3b48a46fc1e1adfa5af1e24
piegames <git@piegames.de>
2020-09-17 23:58:59 +0200

    Minor refactoring: moving things around

--- examples/send-file-blocking.rs
+++ examples/send-file-blocking.rs
@@ -1,5 +1,5 @@
 use magic_wormhole::io::blocking::MessageType;
-use magic_wormhole::io::blocking;
+use magic_wormhole::io::blocking::Wormhole;
 
 // Can ws do hostname lookup? Use ip addr, not localhost, for now
 const MAILBOX_SERVER: &str = "ws://relay.magic-wormhole.io:4000/v1";
@@ -11,14 +11,12 @@ fn main() {
     let mailbox_server = String::from(MAILBOX_SERVER);
     let app_id = String::from(APPID);
 
-    let mut w = blocking::connect(app_id.clone(), mailbox_server);
-    let code = blocking::get_code(&mut w);
+    let mut w = Wormhole::new(&app_id, &mailbox_server);
+    w.allocate_code(2);
+    let code = w.get_code();
     println!("got the code: {}", code);
-
-    // parse the relay url
-    let relay_url = blocking::parse_relay_url(RELAY_SERVER);
 
     // send a file
     let msg = MessageType::File{ filename: "foobar".to_string(), filesize: 40960 };
-    blocking::send(&mut w, app_id, code, msg, &relay_url);
+    w.send(app_id, code, msg, &RELAY_SERVER.parse().unwrap());
 }
