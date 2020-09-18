7823ccc5abfc6929b987b8a289e6bed4bb2142e3
piegames <git@piegames.de>
2020-09-18 13:27:11 +0200

    Error handling and code cleanup
    - Sprinkling around with error handling. I chose the `anyhow` crate because its philosophy matches the existing error handling attempts better, allowing a 1:1 replacement like `panic!` -> `bail!`, `expect!` -> `.context` etc. Howevery, we probably want to switch to `thiserror` in the long run, because that one is not a transitive dependency to users of the wormhole library.
    - Replaced `println!`s with `trace!`.
    - Removed a lot of cloning data, as of #31

--- examples/send-file-blocking.rs
+++ examples/send-file-blocking.rs
@@ -1,5 +1,6 @@
 use magic_wormhole::io::blocking::MessageType;
 use magic_wormhole::io::blocking::Wormhole;
+use log::*;
 
 // Can ws do hostname lookup? Use ip addr, not localhost, for now
 const MAILBOX_SERVER: &str = "ws://relay.magic-wormhole.io:4000/v1";
@@ -7,16 +8,15 @@ const RELAY_SERVER: &str = "tcp:transit.magic-wormhole.io:4001";
 const APPID: &str = "lothar.com/wormhole/text-or-file-xfer";
 
 fn main() {
-    env_logger::try_init().unwrap();
+    env_logger::builder().filter_level(LevelFilter::Trace).init();
     let mailbox_server = String::from(MAILBOX_SERVER);
-    let app_id = String::from(APPID);
 
-    let mut w = Wormhole::new(&app_id, &mailbox_server);
+    let mut w = Wormhole::new(&APPID, &mailbox_server);
     w.allocate_code(2);
     let code = w.get_code();
     println!("got the code: {}", code);
 
     // send a file
-    let msg = MessageType::File{ filename: "foobar".to_string(), filesize: 40960 };
-    w.send(app_id, code, msg, &RELAY_SERVER.parse().unwrap());
+    let msg = MessageType::File{ filename: "example-file.bin".to_string(), filesize: 40960 };
+    w.send(APPID, &code, msg, &RELAY_SERVER.parse().unwrap());
 }
