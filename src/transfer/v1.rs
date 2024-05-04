619a3663ea69f077e579d04654d7afc403fe353d
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-04 19:06:38 -0400

    Update src/transfer/v1.rs
    Co-authored-by: Fina <code@felinira.net>

--- src/transfer/v1.rs
+++ src/transfer/v1.rs
@@ -581,9 +581,7 @@ pub async fn send_records<'a>(
 
     let mut hasher = Sha256::default();
 
-    // Yeah, maybe don't allocate 16kiB on the stack…
-    // Update chunk size from 4KB to 16KB: https://github.com/magic-wormhole/magic-wormhole.rs/issues/224
-    let mut plaintext = Box::new([0u8; 16384]);
+    let mut plaintext = vec![0u8; 16 * 1024].into_boxed_slice();
     let mut sent_size = 0;
     futures::pin_mut!(files);
     while let Some(mut file) = files.next().await.transpose()? {
