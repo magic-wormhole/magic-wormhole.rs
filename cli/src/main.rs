ffdc814a2307df644ae1ad024be9d8770f3a8dfb
Fina Wilke <code@felinira.net>
2025-01-11 01:50:48 +0100

    zsh: Fix completions

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -239,6 +239,7 @@ enum WormholeCommand {
     version,
     author,
     about,
+    name = "wormhole-rs",
     arg_required_else_help = true,
     disable_help_subcommand = true,
     propagate_version = true,
